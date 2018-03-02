72e98f125cd6d0eeddcccc5c8224704a25493e29
druogury <damien.russier@ogury.co>
2018-03-02 18:19:46 +0100

    add some corrections

--- install_anaconda.bash
+++ install_anaconda.bash
@@ -26,9 +26,63 @@ sudo apt install emacs25
 mkdir .emacs.d ; cd .emacs.d ; git init
 git remote add origin https://github.com/drussier/emacs-config.git
 git pull origin master
+
+echo -e $'
+[user]
+        name = drussier
+        email = damien.russier@gmail.com'
+
 echo -e $'#!/bin/bash -vf
 
 et() { /usr/bin/emacs-25.325 -q --load ~/.emacs.d/init.el -nw "$@" ; }
+
+gitpull() {
+if [ -z "$1" ]
+then
+    br="master"
+else
+    br="$1"
+fi
+git pull origin $br
+}
+
+gitpush() {
+if [ -z "$1" ]
+then
+    br="master"
+else
+    br="$1"
+fi
+git push -u origin $br
+}
+
+ipynb2py() {
+jupyter nbconvert --to python $1
+}
+
+blk() {
+mv "$1" `echo $1 | tr ',' '_' | tr ':' '_' | tr ' ' '_'  | tr -s '_'`
+}
+
+alias ipynb="jupyter notebook &"
+alias juplab="jupyter lab &"
+
+########## shortcuts linux
+alias  cal="awk 'BEGIN{print \!*}'"
+alias   xt="xterm -bg black -fg white -fn 10x20 -cm -sl 500 -sb -T Geocluster -e '/bin/csh -l' &"
+alias term="terminator -m &"
+alias   ko="konsole &"
+alias  lsn="ls --color=none -F -nalh"
+alias lsns="ls --color=none -F -nalhS"
+alias  lsr="ls --color=none -F -nalRh" 
+alias lsrs="ls --color=none -F -nalRhS" 
+alias   ll="ls -lhrt"
+alias   hh="history | egrep -i \!*"
+alias  hht="history | egrep -i \!* | tail"
+alias    g="gunzip \!* &"
+alias   al="source ~/.bash_aliases"
+alias edal="et ~/.bash_aliases"
+
 
 ##########git
 alias  gi="git init     "  
@@ -43,6 +97,7 @@ alias grl="git reflog "
 alias grb="git rebase "
 alias glg="git log --graph --decorate --oneline"
 
+##########
 alias py36gensim="source activate py36gensim ; which python"
 alias proj="py36gensim ; cd $HOME/proj/"
 alias appinst="proj ; cd apps_install/src/ ; export PYTHONPATH=$HOME/proj/apps_install/src/ ; pwd"
