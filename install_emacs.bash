9a0edf8f9c6acffdc92ce4c4124cfde8eefbfe17
druogury <damien.russier@ogury.co>
2018-05-18 15:43:09 +0200

    remote gitlab for emacs-config

--- install_emacs.bash
+++ install_emacs.bash
@@ -12,7 +12,7 @@ if [ ! -d "$EMACS_DIR" ]; then
     mkdir ~/.emacs.d
 fi
 cd ~/.emacs.d ; git init
-git remote add origin https://github.com/drussier/emacs-config.git
+git remote add origin https://gitlab.com/druprivate/emacs-config.git # https://github.com/drussier/emacs-config.git
 git pull origin master
 
 
