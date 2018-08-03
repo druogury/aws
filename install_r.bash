8bb1f090aede23c57c692c7a7ee8731062258784
drussier <damien.russier@ogury.co>
2018-08-03 09:47:52 +0000

    add sudo to install R

--- install_r.bash
+++ install_r.bash
@@ -5,12 +5,12 @@ cd $HOME
 
 wget https://mran.blob.core.windows.net/install/mro/$mro/microsoft-r-open-$mro.tar.gz \
    && tar -xvf microsoft-r-open-$mro.tar.gz \
-   && ./microsoft-r-open/install.sh -au \
+   && sudo ./microsoft-r-open/install.sh -au \
    && rm -rf /tmp/*
 
 # fix things
 wget -q -O /tmp/libpng12.deb http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb \
-   && dpkg -i /tmp/libpng12.deb \
+   && sudo dpkg -i /tmp/libpng12.deb \
    && rm /tmp/libpng12.deb
 
 exit 0
