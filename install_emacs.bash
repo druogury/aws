592d1cafa1c34ab24fef5fe0f37a16bf5926ae56
druogury <36401785+druogury@users.noreply.github.com>
2018-09-28 16:09:57 +0200

    Update install_emacs.bash

--- install_emacs.bash
+++ install_emacs.bash
@@ -2,35 +2,12 @@
 
 EMACS_DIR=${HOME}/.emacs.d
 
-# https://askubuntu.com/questions/851633/emacs-25-on-ubuntu-16-10
-sudo add-apt-repository ppa:kelleyk/emacs
-sudo apt update
-sudo apt install -y emacs25
-/usr/bin/emacs-25.325 --version
-
 if [ ! -d "$EMACS_DIR" ]; then
     mkdir ~/.emacs.d
 fi
-cd ~/.emacs.d ; git init
-git remote add origin https://gitlab.com/druprivate/emacs-config.git # https://github.com/drussier/emacs-config.git
+cd $EMACS_DIR ; git init
+git remote add origin https://gitlab.com/druprivate/emacs-config.git
 git pull origin master
-
-
-echo -e $'
-##########
-EMACS="/usr/bin/emacs-25.325 -q --load ~/.emacs.d/init.el -nw"
-
-et() { $EMACS "$@" ; }
-etd() { $EMACS "$@" --debug-init ; }
-# emacs client in terminal '-t' == '-nw' from 'man emacsclient')
-ec() { emacsclient -t ; }
-# https://unix.stackexchange.com/questions/73484/how-can-i-set-vi-as-my-default-editor-in-unix
-export VISUAL=$EMACS
-export EDITOR="$VISUAL"
-' >> ~/.bash_aliases
-
-# start/stop the Emacs server
-alias emstart="emacs --daemon -q --load ~/.emacs.d/init.el"
-alias emstop="emacsclient -e '(kill-emacs)'" # '-e' == eval
+./install.bash
 
 exit 0
